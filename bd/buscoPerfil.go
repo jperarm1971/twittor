35553b0ce2f7dfe30f5dda9bae563298d2a2bb96
Josué <josue@MacBook-Pro-de-Josue.local>
2021-09-26 12:22:37 +0100

    busco perfil

---
+++ bd/buscoPerfil.go
@@ -0,0 +1,30 @@
+package bd
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/jperarm1971/twittor/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func BuscoPerfil(ID string) (models.Usuario, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*15)
+	defer cancel()
+	db := MongoCN.Database("twittor")
+	col := db.Collection("usuarios")
+	var perfil models.Usuario
+	objID, _ := primitive.ObjectIDFromHex(ID)
+	condicion := bson.M{
+		"_id": objID,
+	}
+	err := col.FindOne(ctx, condicion).Decode(&perfil)
+	perfil.Password = ""
+	if err != nil {
+		fmt.Println("Se produjo en error la búsqueda del registro de usuario" + err.Error())
+		return perfil, err
+	}
+	return perfil, nil
+}
